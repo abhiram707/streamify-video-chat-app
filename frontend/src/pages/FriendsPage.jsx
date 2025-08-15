61f37031505f676c4c092373a9d6641f1529610a
abhiram707 <abhiramreddylucky@gmail.com>
2025-08-16 00:22:18 +0530

    Create FriendsPage.jsx

---
+++ frontend/src/pages/FriendsPage.jsx
@@ -0,0 +1,11 @@
+const FriendsPage = () => {
+  // In future, you can fetch friends list here via API
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Friends</h1>
+      <p>This is where your friends list will appear.</p>
+    </div>
+  );
+};
+
+export default FriendsPage;
