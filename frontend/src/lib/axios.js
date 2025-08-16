4b07505d1b0b7577e9768fbe6e0aa8d1a216a859
abhiram707 <abhiramreddylucky@gmail.com>
2025-08-16 10:10:40 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
-const BASE_URL=import.meta.env.MODE==="development"?"http://localhost:5001/api":"/api"
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api"
+    : import.meta.env.VITE_API_URL; // use env var for production
 
-export const axiosInstance=axios.create({
-    baseURL:BASE_URL,
-    withCredentials:true
-})
+export const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true,
+});
