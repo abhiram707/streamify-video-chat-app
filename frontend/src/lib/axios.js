10ab7fb771b197a0cd3f384158c22307828418dc
abhiram707 <abhiramreddylucky@gmail.com>
2025-09-05 11:16:33 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,12 +1,11 @@
-// src/lib/axios.js
 import axios from "axios";
 
 const BASE_URL =
   import.meta.env.MODE === "development"
-    ? "http://localhost:5001/api" // Local backend
-    : "https://streamify-video-chat-app-6.onrender.com/api"; // Render backend in production
+    ? "http://localhost:5001/api"
+    : "https://streamify-video-chat-app-6.onrender.com/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
-  withCredentials: true, // ✅ ensures cookies (JWT) are sent
+  withCredentials: true, // important for cookies/JWT
 });
