f2039ac1af37e2b458e4973ff80b2a93179324f3
abhiram707 <abhiramreddylucky@gmail.com>
2025-09-05 11:15:03 +0530

    Update chat.route.js

--- backend/src/routes/chat.route.js
+++ backend/src/routes/chat.route.js
@@ -1,11 +1,10 @@
-// src/routes/chat.route.js
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
 import { getStreamToken } from "../controllers/chat.controller.js";
 
 const router = express.Router();
 
-// 🎟️ Generate Stream token (protected)
+// Protected token route
 router.get("/token", protectRoute, getStreamToken);
 
 export default router;
