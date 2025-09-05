d2532ddcf996748dd89fdcc877f64e7bd7f3e4a3
abhiram707 <abhiramreddylucky@gmail.com>
2025-09-05 11:15:40 +0530

    Update chat.controller.js

--- backend/src/controllers/chat.controller.js
+++ backend/src/controllers/chat.controller.js
@@ -1,12 +1,9 @@
-// src/controllers/chat.controller.js
 import { StreamChat } from "stream-chat";
 
-// 🎟️ Generate Stream token (for Chat + Video SDK)
 export const getStreamToken = async (req, res) => {
   try {
     const userId = req.user._id.toString();
 
-    // Use your Stream API key & secret from env
     const chatClient = StreamChat.getInstance(
       process.env.STREAM_API_KEY,
       process.env.STREAM_API_SECRET
